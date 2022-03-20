b9723255040bb4155c2206b538801c014ba5916d
mertsancar <mertsancar12@gmail.com>
2022-03-20 19:24:18 +0300

    Done SetGradeForStudent()
    Co-Authored-By: NisanurCandan <93336227+nisanurcandan@users.noreply.github.com>

--- StudentManagementSystem/StudentManagementSystem/Pages/SetGradePage/SetGradeController.cs
+++ StudentManagementSystem/StudentManagementSystem/Pages/SetGradePage/SetGradeController.cs
@@ -18,76 +18,33 @@ namespace StudentManagementSystem.Pages.SetGradePage
         public void SetGradeScreen()
         {
             Professor currentProf = (Professor)CurrentUser.currentUser;
-            currentProf.ShowMyAcademicInfo();
 
+            currentProf.ShowMyAcademicInfo();
             Console.WriteLine("Choose a lecture");
             int lectureIndex = int.Parse(Console.ReadLine());
             Lecture selectedLecture = currentProf.Lectures[lectureIndex - 1];
-            List<Student> takeCourseStudents = new List<Student>();
-            for (int i = 0; i < DefaultData.students.Count; i++)
-            {
-                if(DefaultData.students[i].Lectures.Contains(selectedLecture))
-                {
-                    takeCourseStudents.Add(DefaultData.students[i]);
-                }
-                  
-            }
+            List<Student> takeCourseStudents = SetGradeManager._instance.GetStudentsByLecture(selectedLecture);
 
             Console.Clear();
             for (int i = 0; i < takeCourseStudents.Count; i++)
             {
                 Console.WriteLine("Index: " + (i + 1) + " ID: " + takeCourseStudents[i].id + " Name: " + takeCourseStudents[i].fullName );
             }
-
             Console.WriteLine("Choose a student");
             int studentIndex = int.Parse(Console.ReadLine());
             Student selectedStudent = takeCourseStudents[studentIndex - 1];
+
             Console.Clear();
             Console.WriteLine("Enter a grade for student");
             int grade = int.Parse(Console.ReadLine());
+            LetterGrades newLetterGrade = SetGradeManager._instance.NumberGradeToLetterGrade(grade);
+            selectedStudent.Grades[selectedLecture] = newLetterGrade;
+            selectedStudent.CalculateGPA();
 
-            if (grade > 90)
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.AA;
-            }
-            else if (grade > 85 && 90 < grade)
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.BA;
-            }
-            else if (grade > 80 && 85 < grade)
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.BB;
-            }
-            else if (grade > 75 && 80 < grade)
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.CB;
-            }
-            else if (grade > 60 && 75 < grade)
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.CC;
-            }
-            else if (grade > 55 && 60 < grade)
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.DC;
-            }
-            else if (grade > 50 && 55 < grade)
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.DD;
-            }
-            else
-            {
-                selectedStudent.Grades[selectedLecture] = LetterGrades.FF;
-            }
-
-
-
-
-
-
+            Console.WriteLine("Grade was successfully changed");
+            string ans = Console.ReadLine();
+            MainScreenController._instance.MainScreen();
         }
-
-
-
 
     }
 }
